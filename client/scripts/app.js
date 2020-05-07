a88a347f93b89c4c872ed214b051c768ae0ecc73
Chris Palka <cpalka87gmail.com>
2020-05-06 22:16:06 -0600

    Initial refactor of rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -21,18 +21,34 @@ var App = {
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
-      console.log(data);
+      // console.log(data);
       $('#chats').empty();
       var html = '';
+      var roomHtml = '';
+      var roomObj = {};
+      var roomArray = [];
+      var roomArrayTwo = [];
       for (var i = 0; i < data.results.length; i++) {
         const username = data.results[i].username;
         const text = data.results[i].text;
         const roomname = data.results[i].roomname;
         if (username !== undefined && text !== undefined && roomname !== undefined) {
           html += MessageView.render(data.results[i]);
+          if (!roomArray.includes(roomname) && roomname !== '') {
+            roomArray.push(roomname);
+          }
         }
         $('#chats').append(html);
       }
+      for (var r = 0; r < roomArray.length; r++) {
+        var roomObj = {
+          roomname: roomArray[r]
+        };
+        roomArrayTwo.push(roomObj);
+        roomHtml += RoomsView.render(roomArrayTwo[r]);
+      }
+      $('#rooms select').append(roomHtml);
+      console.log(roomArrayTwo);
       callback();
     });
   },
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -6,7 +6,25 @@ var RoomsView = {
   initialize: function() {
   },
 
-  render: function() {
-  }
 
+  render: _.template(
+    `<option value="<%= roomname %>">
+      <%= roomname %>
+    </option>
+    `
+  )
+
+// <option value="room1">ROOM1</option>
 };
+
+
+// var filterRooms = function(rooms) {
+//   var roomArray = [];
+//   for (var i = 0; i < rooms.length; i++) {
+//     var currentRoom = rooms[i];
+//     if (!roomArray.includes(currentRoom)) {
+//       roomArray.push(currentRoom);
+//     }
+//   }
+//   return roomArray;
+// };
